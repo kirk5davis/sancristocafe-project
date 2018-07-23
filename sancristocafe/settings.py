81e497e920e444f1c40fa30aaa59bace4cf96c1a
Kirk Davis <kirk5davis@gmail.com>
2018-07-23 03:12:32 -0600

    added local_settings support

--- sancristocafe/settings.py
+++ sancristocafe/settings.py
@@ -137,3 +137,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
