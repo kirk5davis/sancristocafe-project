008efeca76e72716931fc3e3fd24f6ae63bb26cd
Kirk Davis <kirk5davis@gmail.com>
2019-01-06 13:35:54 -0800

    clean up and IE support

--- sancristocafe/static/js/main.js
+++ sancristocafe/static/js/main.js
@@ -4,15 +4,21 @@
 $(document).ready(function(){
  $('.header-of-site').height($(window).height());
 });
-
-//if the page gets resized change the height of the header to match that space
+//
+////if the page gets resized change the height of the header to match that space
 $(window).on('resize',function(){
 	$('.header-of-site').height($(window).height())
 });
 
-//animate the home page
-$(".navbar a").click(function(){
-  $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top
-  },1000)
+// flipping the about us images
+$('.flip').hover(function(){
+        $(this).find('.card').toggleClass('flipped');
  });
+
+$('.flip-card').hover(function(){$('.flip-card').toggleClass('applyflip');}.bind(this));
+////animate the home page
+//$(".navbar a").click(function(){
+//  $("body,html").animate({
+//   scrollTop:$("#" + $(this).data('value')).offset().top
+//  },1000)
+// });
