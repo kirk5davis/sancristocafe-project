54654078dd774454d6c1ecd08eee65375bbc5b90
Kirk Davis <kirk5davis@gmail.com>
2019-01-24 12:54:51 -0800

    collected static files to show css/js changes

--- static/js/main.js
+++ static/js/main.js
@@ -11,18 +11,39 @@ $(window).on('resize',function(){
 });
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById('main').style.top = '0'
-  } else {
-    document.getElementById("navbar").style.top = "-60px";
-    document.getElementById('main').style.margin-top = '-60px'
-  }
-  prevScrollpos = currentScrollPos;
-}
+$(function () {
+  var lastScrollTop = 0;
+  var $navbar = $('.navbar');
+
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+
+    if (st > lastScrollTop) { // scroll down
+
+      // use this is jQuery full is used
+      $navbar.fadeOut()
+
+      // use this to use CSS3 animation
+      // $navbar.addClass("fade-out");
+      // $navbar.removeClass("fade-in");
+
+      // use this if no effect is required
+      // $navbar.hide();
+    } else { // scroll up
+
+      // use this is jQuery full is used
+      $navbar.fadeIn()
+
+      // use this to use CSS3 animation
+      // $navbar.addClass("fade-in");
+      // $navbar.removeClass("fade-out");
+
+      // use this if no effect is required
+      // $navbar.show();
+    }
+    lastScrollTop = st;
+  });
+});
 
 
 // flipping the about us images
