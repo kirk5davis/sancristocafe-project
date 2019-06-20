ffa3afaf60924359cefa3823d0d2b71203a7c776
Kirk Davis <kirk5davis@gmail.com>
2019-06-19 23:09:46 -0700

    in-between step of logo/color palette inclusion

--- static/js/main.js
+++ static/js/main.js
@@ -100,34 +100,6 @@ Barba.Dispatcher.on('transitionCompleted', function(currentStatus, oldStatus, co
 
 });
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-$(function () {
-  var lastScrollTop = 0;
-  var $navbar = $('.navbar');
-
-  $(window).scroll(function(event){
-    var st = $(this).scrollTop();
-    if (st > lastScrollTop) { // scroll down
-      // use this is jQuery full is used
-      // $navbar.fadeOut()
-      // use this to use CSS3 animation
-      $navbar.addClass("fade-out");
-      $navbar.removeClass("fade-in");
-      // use this if no effect is required
-      // $navbar.hide();
-    } else { // scroll up
-      // use this is jQuery full is used
-      // $navbar.fadeIn()
-      // use this to use CSS3 animation
-      $navbar.addClass("fade-in");
-      $navbar.removeClass("fade-out");
-      // use this if no effect is required
-      // $navbar.show();
-    }
-    lastScrollTop = st;
-  });
-});
-
 
 //if the page gets resized change the height of the header to match that space
 $(window).on('resize',function(){
