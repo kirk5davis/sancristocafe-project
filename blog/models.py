8b26be1a90dae561be1e50bdb7161f970b299c2a
Kirk Davis <kirk5davis@gmail.com>
2019-08-30 13:24:56 -0700

    added image size reduction on upload for Coffee and BlogPost models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from io import BytesIO
+from PIL import Image as Img
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # blog model
 class Blog(models.Model):
@@ -8,6 +11,19 @@ class Blog(models.Model):
 	summary = models.CharField(max_length=500)
 	content = models.TextField()
 	created_on = models.DateTimeField(blank=False, help_text="This will sort the new posts, the newest one will be featured")
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			img = Img.open(BytesIO(self.image.read()))
+			if img.mode != 'RGB':
+				img = img.convert('RGB')
+			new_width = 800
+			img.thumbnail((new_width, new_width * self.image.height / self.image.width), Img.ANTIALIAS)
+			output = BytesIO()
+			img.save(output, format='JPEG', quality=70)
+			output.seek(0)
+			self.image= InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.image.name.split('.')[0], 'image/jpeg', output.write, None)
+		super(Blog, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return "News - {}".format(self.title)
--- coffee/models.py
+++ coffee/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from io import BytesIO
+from PIL import Image as Img
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # coffees that san cristobal offers
 class Coffee(models.Model):
@@ -8,6 +11,19 @@ class Coffee(models.Model):
 	coffee_name = models.CharField(max_length=100, blank=False)
 	bag_tag = models.CharField(max_length=10, blank=False, help_text="Put the exact bag tag here, it creates button hyperlinks!")
 	description = models.TextField(help_text="Write out exactly how you would like it to show on the coffee tile!")
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			img = Img.open(BytesIO(self.image.read()))
+			if img.mode != 'RGB':
+				img = img.convert('RGB')
+			new_width = 800
+			img.thumbnail((new_width, new_width * self.image.height / self.image.width), Img.ANTIALIAS)
+			output = BytesIO()
+			img.save(output, format='JPEG', quality=70)
+			output.seek(0)
+			self.image= InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.image.name.split('.')[0], 'image/jpeg', output.write, None)
+		super(Coffee, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return "Coffee - {}".format(self.coffee_name)
