ab665614cc19ec0eb7c857a5785daa08dc679663
Kirk Davis <kirk5davis@gmail.com>
2020-04-06 17:06:51 -0700

    editing email confirmation slightly

--- contact/views.py
+++ contact/views.py
@@ -19,8 +19,7 @@ def emailView(request):
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
             try:
-                text = "<h2 style='color: red;'> <small>New message from the sancristocafe.com contact-us form! </small><br> \
-                        {0} is reaching out to us...</h2>\
+                text = "<h2 style='color: red;'> <small>New message from the sancristocafe.com contact-us form! </small><br></h2>\
                             <p>************************************************************</p>\
                             <p><b>Name:</b> {0}</p> \
                             <p><b>Email:</b> {2}</p>   \
@@ -29,7 +28,7 @@ def emailView(request):
                                 <p><small> This contact form was submitted at {1} <br> \
                                 <b>be sure to follow-up and have a great day! <br>--your sancristocafe.com site </small> </b></p> \
                                      ".format(name, time.ctime(), email, message)
-                send_html_mail("[NOTICE] sancristocafe.com contact-us form submission from: {}".format(name), text, ['kirk5davis@gmail.com'], settings.EMAIL_HOST_USER)
+                send_html_mail("[NOTICE] sancristocafe.com contact-us submission: {}".format(name), text, settings.CONTACT_US_NOTIFICATION_EMAIL_LIST, settings.EMAIL_HOST_USER)
                 form.save()
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
